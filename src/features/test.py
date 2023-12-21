8fd820579f864e6d3298c3d00a8967dd7cf327e7
zhaiqt <zhaiqt@hotmail.com>
2023-12-21 12:50:48 -0800

    first commit

---
+++ src/features/test.py
@@ -0,0 +1,14 @@
+def create_list():
+    item_list = [
+        "rice",
+        "chicken",
+        "egg",
+        "hotdog",
+        "bean",
+    ]
+    return item_list
+
+
+item_list = create_list()
+for item in item_list:
+    print(item)
